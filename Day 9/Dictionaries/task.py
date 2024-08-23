ce61fdaed34b4bea3c5f87ae073029006554db61
Roshanking021 <roshanking021@gmail.com>
2024-08-23 16:24:07 +0530

    Day 9 of learning python

---
+++ Day 9/Dictionaries/task.py
@@ -0,0 +1,11 @@
+age = 20
+programming_dictionary = {
+   "name":"Roshan",
+    age:age,
+    "profession": "web-developer"
+}
+# del programming_dictionary
+
+# for not getting errors
+# removed_key = programming_dictionary.pop("agaee", "key not found")
+# print(removed_key)
