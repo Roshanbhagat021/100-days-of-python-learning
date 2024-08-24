18a546590c73f58cc46550c922e378e252717219
Roshanking021 <roshanking021@gmail.com>
2024-08-24 22:34:17 +0530

    added docstring

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,4 +1,6 @@
 def formate_name(f_name, l_name):
+     """ a function for converting a string into title case
+     """
      return f"{f_name.title()} {l_name.title()}"
 x = formate_name("roshan", "kuMaR")
-print(x)
+print(formate_name.__doc__)
