b758dfbe0b448ab18c9eb2a8317ec0c69ce53e62
Roshanking021 <roshanking021@gmail.com>
2024-08-25 15:20:28 +0530

    added the calculator project

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,46 @@
+from art import  logo
 def add(n1, n2):
     return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations ={
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+
+
+def perform(input1):
+    for operation in operations:
+        print(operation)
+    op = None
+    while True:
+         op = input("Pick an operation: ")
+         if len(op) != 1 or op not in operations:
+             print("Please choose the correct operation")
+         else:
+             break
+    second_input = float(input("What is the next number?: "))
+    result = operations[op](input1, second_input)
+    print(f"{input1} {op} {second_input} = {result }")
+
+    do_more = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower()
+    if do_more == "y" :
+        return perform(result)
+
+print(logo)
+while True:
+    f_input = float(input("What is the first number?: "))
+    perform(f_input)
 
 
--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,6 +1,5 @@
 def formate_name(f_name, l_name):
-     """ a function for converting a string into title case
-     """
+     """A function for converting strings into title case"""
      return f"{f_name.title()} {l_name.title()}"
 x = formate_name("roshan", "kuMaR")
-print(formate_name.__doc__)
+print(x)
