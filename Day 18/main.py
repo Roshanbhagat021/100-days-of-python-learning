deba07de6bbeaae09cf2501691555859e873d055
Roshanking021 <roshanking021@gmail.com>
2024-09-02 23:54:32 +0530

    Turtle library

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,29 +1,46 @@
 from turtle import Turtle, Screen
+import random
 
 t = Turtle()
-tom = Turtle()
+# tom = Turtle()
 t.shape("turtle")
 
 t.color("indianred1")
 
-t.left(90)
-def create_square():
-    for _ in range(4):
+
+# Challange one
+# t.left(90)
+# def create_square():
+#     for _ in range(4):
+#         t.forward(100)
+#         t.right(90)
+
+
+# create_square()
+
+
+
+# Challange two
+
+# for _ in range(15):
+#     t.speed(1)
+#     t.forward(10)
+#     t.penup()
+#     t.forward(10)
+#     t.pendown()
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black"]
+
+
+def draw_shapes(no_of_sides):
+    angle = 360/no_of_sides
+    for _ in range(no_of_sides):
         t.forward(100)
-        t.right(90)
+        t.right(angle)
 
 
-create_square()
-
-
-
-
-
-
-
-
-
-
+for shape_size_n in range(3,11):
+    draw_shapes(shape_size_n)
 
 
 screen = Screen()
